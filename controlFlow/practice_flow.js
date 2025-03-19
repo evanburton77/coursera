cb22d61eeb8891f245aaf3db7b5ad80e4084110b
evanburton77 <evanburton77@gmail.com>
2025-03-18 20:24:18 -0400

    practice done

---
+++ controlFlow/practice_flow.js
@@ -0,0 +1,13 @@
+let userRole = "none"
+let accessLevel
+if (userRole === "Employee"){
+accessLevel = "You have access to Dietary Services";
+} else if (userRole === "Enrolled Member"){
+    accessLevel = "You have access to both Dietary Services and one-on-one interaction with a dietician";
+} else if (userRole === "Subscriber") {
+    accessLevel = "You have partial access to facilitate Dietary Services only";
+} else {
+    accessLevel = "You need to enroll or at least subscribe";
+}
+
+console.log("What you get:", accessLevel)
