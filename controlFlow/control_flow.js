2f4c3a2a102d9da7a7df7546c7e6c9edf1b5f821
evanburton77 <evanburton77@gmail.com>
2025-03-18 20:10:49 -0400

    done control flow

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,3 +42,8 @@ switch (userType) {
         userCategory = "Unknown";
 }
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated"
+
+console.log("Authentication Status:", authenticationStatus)
